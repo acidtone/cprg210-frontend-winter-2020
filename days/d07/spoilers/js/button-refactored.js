d35838d82b491ba493b17a6448f1473df8d6aa2b
Tony Grimes <acidtone@tonygrimes.com>
2020-02-14 15:07:00 -0700

    Added fixed refactored code

--- days/d07/spoilers/js/button-refactored.js
+++ days/d07/spoilers/js/button-refactored.js
@@ -2,22 +2,18 @@ function init() {
   // Initialize our element variables
   const body = document.querySelector('body');
   const button = document.querySelector('button');
-  const counter = document.querySelector('span');
 
-  button.addEventListener('click',function(){
+  button.addEventListener('click',function(evt){
     // Find the current number of times clicked (hint: try innerText) and assign it to a variable called 'currentCount'.
+    const counter = document.querySelector('span');
+
     let currentCount = counter.innerText;
 
-    // Iterate and udpate the counter using the method we learned yesterday (*cough*innerText*cough*).
-    counter.innerText = currentCount++;
+    // Increment and udpate the counter using the method we learned yesterday (*cough*innerText*cough*).
+    currentCount++;
+    counter.innerText = currentCount;
     
     // Toggle the body background: add the 'active' class when it's missing and remove it when it's present.
-
-    // if (body.classList.contains('active')) {
-    //   body.classList.remove('active');
-    // } else {
-    //   body.classList.add('active');
-    // }
     body.classList.toggle('active');
 
     // Bonus refactor step: Try preloading the next image in the sequence to remove the delay from downloading.
